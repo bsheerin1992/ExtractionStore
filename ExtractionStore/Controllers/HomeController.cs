993e8085ca8c71a6dc2159a2b4da43b10273e8eb
Brendan Sheerin <bpsheeri@go.olemiss.edu>
2017-04-26 18:33:03 -0500

    Add project files.

---
+++ ExtractionStore/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExtractionStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Upload()
+        {
+            ViewBag.Message = "Your application upload page.";
+
+            return View();
+        }
+
+        public ActionResult Analysis()
+        {
+            ViewBag.Message = "Your analysis page.";
+
+            return View();
+        }
+    }
+}
---
+++ ExtractionStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExtractionStore.Startup))]
+namespace ExtractionStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
