69318c8914c1e9963b5ade76ab64dc524a12f5c2
Brendan Sheerin <bpsheeri@go.olemiss.edu>
2017-05-04 14:01:53 -0500

    -added FileController with views -Analysis page changes     -search bar to Ajax     -added partial view -added bundles

--- ExtractionStore/App_Start/BundleConfig.cs
+++ ExtractionStore/App_Start/BundleConfig.cs
@@ -8,8 +8,17 @@ namespace ExtractionStore
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/exst").Include(
+                       "~/Scripts/jquery-{version}.js",
+                       "~/Scripts/jquery-ui-{version}.js",
+                       "~/Scripts/jquery.unobtrusive*",
+                       "~/Scripts/jquery.validate*"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/jquery-ui-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -26,6 +35,9 @@ namespace ExtractionStore
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+                      "~/Content/themes/base/jquery-ui.css"));
         }
     }
 }
---
+++ ExtractionStore/Controllers/FileController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ExtractionStore.Models;
+
+namespace ExtractionStore.Controllers
+{
+    public class FileController : Controller
+    {
+        private ExtractionStoreDb db = new ExtractionStoreDb();
+
+        // GET: File
+        public ActionResult Index(string searchTerm = null)
+        {
+            var model =
+                            db.Files
+                               .OrderBy(f => f.Name)
+                               .Where(f => searchTerm == null || f.Name.Contains(searchTerm))
+                               .Take(10);
+
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView("_Files", model);
+            }
+
+            return View(model);
+        }
+
+        // GET: File/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            File file = db.Files.Find(id);
+            if (file == null)
+            {
+                return HttpNotFound();
+            }
+            return View(file);
+        }
+
+        // GET: File/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: File/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Type,Data")] File file)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Files.Add(file);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(file);
+        }
+
+        // GET: File/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            File file = db.Files.Find(id);
+            if (file == null)
+            {
+                return HttpNotFound();
+            }
+            return View(file);
+        }
+
+        // POST: File/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Type,Data")] File file)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(file).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(file);
+        }
+
+        // GET: File/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            File file = db.Files.Find(id);
+            if (file == null)
+            {
+                return HttpNotFound();
+            }
+            return View(file);
+        }
+
+        // POST: File/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            File file = db.Files.Find(id);
+            db.Files.Remove(file);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
