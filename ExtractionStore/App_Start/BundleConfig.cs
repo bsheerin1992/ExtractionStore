c6fab467219fd67746dde669efd94dcfe30ac874
Brendan Sheerin <bpsheeri@go.olemiss.edu>
2017-05-04 16:19:11 -0500

    -created script for asynchronous search and added it to exst bundle -made search on analysis page/file index asynchronous

--- ExtractionStore/App_Start/BundleConfig.cs
+++ ExtractionStore/App_Start/BundleConfig.cs
@@ -12,7 +12,9 @@ namespace ExtractionStore
                        "~/Scripts/jquery-{version}.js",
                        "~/Scripts/jquery-ui-{version}.js",
                        "~/Scripts/jquery.unobtrusive*",
-                       "~/Scripts/jquery.validate*"));
+                       "~/Scripts/jquery.validate*",
+                       "~/Scripts/exst-async.js"
+                       ));
 
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
